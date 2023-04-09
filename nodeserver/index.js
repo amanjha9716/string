ece4f4f779c4227e6364af773cb87356c8b9c59b
amanjha9716 <61967599+amanjha9716@users.noreply.github.com>
2023-04-09 18:44:54 +0530

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,3 +1,28 @@
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 3000;
+
+const user = {};
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    console.log('hi', name);
+    user[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
+
+  socket.on('send', message => {
+    socket.broadcast.emit('recieve', {
+      message: message,
+      name: user[socket.id],
+
 const port = process.env.PORT;
 const io= require('socket.io')(port);
 const user={};
@@ -7,14 +32,17 @@ io.on('connection', socket=>{
         console.log("hi",name);
         user[socket.id]=name;
         socket.broadcast.emit('user-joined',name);
+
     });
-    socket.on('send',message=>{
-        socket.broadcast.emit('recieve',{message:message,name:user[socket.id]});
-    });
-    socket.on('disconnect',name=>{
-        socket.broadcast.emit('left',user[socket.id]);
-        delete user[socket.id];
-        console.log(name);
-        
-    });
-}); 
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('left', user[socket.id]);
+    delete user[socket.id];
+    console.log('user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
