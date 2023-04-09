a71c5202f56b80ed35da8a55e52ae1613de66f83
aman <jhaa2600@gmail.com>
2023-04-09 18:40:40 +0530

    first commit

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,19 +1,36 @@
-const io= require('socket.io')(3000);
-const user={};
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
-io.on('connection', socket=>{
-    socket.on('new-user', name=>{
-        console.log("hi",name);
-        user[socket.id]=name;
-        socket.broadcast.emit('user-joined',name);
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 3000;
+
+const user = {};
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    console.log('hi', name);
+    user[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
+
+  socket.on('send', message => {
+    socket.broadcast.emit('recieve', {
+      message: message,
+      name: user[socket.id],
     });
-    socket.on('send',message=>{
-        socket.broadcast.emit('recieve',{message:message,name:user[socket.id]});
-    });
-    socket.on('disconnect',name=>{
-        socket.broadcast.emit('left',user[socket.id]);
-        delete user[socket.id];
-        console.log(name);
-        
-    });
-}); 
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('left', user[socket.id]);
+    delete user[socket.id];
+    console.log('user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
